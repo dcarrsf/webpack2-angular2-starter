ab96890134fa05f85be202ef8d34b212cfcfc009
dcarrsf <dan@dancarr.co>
2017-06-25 15:13:17 -0700

    Adding comments to defualt routes.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,14 @@
 import { RouterModule, Routes } from '@angular/router';
 
+// Route components (Pages)
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
+// Paths
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent}
 ];
 
+// Export routes
 export const routing = RouterModule.forRoot(routes);
